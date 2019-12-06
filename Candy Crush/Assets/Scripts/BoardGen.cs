9fd1e7d5a89b48caab140c2fc4b8bfb7e686988c
Maxwell Burgess <mjadeb7@gmail.com>
2019-12-06 12:29:23 -0800

    Please enter the commit message for your changes. Lines starting

---
+++ Candy Crush/Assets/Scripts/BoardGen.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardGen : MonoBehaviour
+{
+
+    public Vector2 BoardSize;
+    public Vector2 TopLeft;
+    public Vector2 cellSize;
+    public GameObject[] spriteList;
+    public Vector2 StartTile()
+    {
+
+        //topleft
+        int x, y;
+        x = (int)(-(BoardSize.x / 2));
+        y = (int)(BoardSize.y / 2);
+        return new Vector2(x, y);
+    }
+
+
+    public void AddSprite()
+    {
+
+    }
+   
+    void Start()
+    {
+        //int board;
+        transform.localScale = new Vector3(BoardSize.x, BoardSize.y);
+        TopLeft = StartTile();
+        
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Candy Crush/Assets/Scripts/Sprites.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprites : MonoBehaviour
+{
+    public List<Sprite> sprites = new List<Sprite>(5);
+    public SpriteRenderer spriteRenderer;
+    public int shape;
+    public 
+    enum Spirtes
+    { Square,Circle,Diamond,Star,Hexagon }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shape = (int)Random.Range(0, 4);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
