527d3b084638ee4f19f523a1a5b4f354bdaf87e4
Maxwell Burgess <mjadeb7@gmail.com>
2019-12-13 09:14:29 -0800

    Please enter the commit message for your changes. Lines starting

---
+++ Candy Crush/Assets/Scripts/Score.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Score : MonoBehaviour
+{
+    public static Score instance;
+
+    public GameObject gameOverPanel;
+    public Text yourScoreTxt;
+    public Text highScoreTxt;
+    public Button Rest;
+    public Text scoreTxt;
+    public Text moveCounterTxt;
+
+    private int score, moveCounter;
+
+    void Awake()
+    {
+        instance = GetComponent<Score>();
+        moveCounter = 1;
+    }
+
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+
+        if (score > PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            highScoreTxt.text = "New Best: " + PlayerPrefs.GetInt("HighScore").ToString();
+        }
+        else
+        {
+            highScoreTxt.text = "Best: " + PlayerPrefs.GetInt("HighScore").ToString();
+        }
+
+        yourScoreTxt.text =  "Score: " + score.ToString();
+    }
+    public void Resets()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public int ScoreBoard
+    {
+        get
+        {
+            return score;
+        }
+
+        set
+        {
+            score = value;
+            scoreTxt.text = score.ToString();
+        }
+    }
+
+    public int MoveCounter
+    {
+        get
+        {
+            return moveCounter;
+        }
+
+        set
+        {
+            moveCounter = value;
+            if (moveCounter <= 0)
+            {
+                moveCounter = 0;
+                StartCoroutine(WaitForShifting());
+            }
+            moveCounterTxt.text = moveCounter.ToString();
+        }
+    }
+
+    private IEnumerator WaitForShifting()
+    {
+        yield return new WaitUntil(() => !BoardGen.instance.IsShifting);
+        yield return new WaitForSeconds(.25f);
+        GameOver();
+    }
+}
--- Candy Crush/Assets/Scripts/Sprites.cs
+++ Candy Crush/Assets/Scripts/Sprites.cs
@@ -4,25 +4,146 @@ using UnityEngine;
 
 public class Sprites : MonoBehaviour
 {
-    public List<Sprite> sprites = new List<Sprite>(5);
-    public SpriteRenderer spriteRenderer;
-    public int shape;
-    public 
-    enum Spirtes
-    { Square,Circle,Diamond,Star,Hexagon }
+    private static Color selectedColor = new Color(.5f, .5f, .5f, 1.0f);
+    private static Sprites previousSelected = null;
 
+    private SpriteRenderer render;
+    private bool isSelected = false;
 
+    private Vector2[] adjacentDirections = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        shape = (int)Random.Range(0, 4);
-        
+        render = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Select()
+    {
+        isSelected = true;
+        render.color = selectedColor;
+        previousSelected = gameObject.GetComponent<Sprites>();
+       
+    }
+
+    private void Deselect()
+    {
+        isSelected = false;
+        render.color = Color.white;
+        previousSelected = null;
+    }
+
+    void OnMouseDown()
     {
         
+        if (render.sprite == null || BoardGen.instance.IsShifting)
+        {
+            return;
+        }
+
+        if (isSelected)
+        {
+            Deselect();
+        }
+        else
+        {
+            if (previousSelected == null)
+            { 
+                Select();
+            }
+            else
+            {
+                if (GetAllAdjacentTiles().Contains(previousSelected.gameObject))
+                { 
+                    SwapSprite(previousSelected.render);
+                    previousSelected.ClearAllMatches();
+                    previousSelected.Deselect();
+                    ClearAllMatches();
+                }
+                else
+                {
+                    previousSelected.GetComponent<Sprites>().Deselect();
+                    Select();
+                }
+            }
+        }
     }
+
+    public void SwapSprite(SpriteRenderer render2)
+    {
+        if (render.sprite == render2.sprite)
+        {
+            return;
+        }
+
+        Sprite tempSprite = render2.sprite;
+        render2.sprite = render.sprite;
+        render.sprite = tempSprite;
+        
+       
+    }
+
+    private GameObject GetAdjacent(Vector2 castDir)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir);
+        if (hit.collider != null)
+        {
+            return hit.collider.gameObject;
+        }
+        return null;
+    }
+
+    private List<GameObject> GetAllAdjacentTiles()
+    {
+        List<GameObject> adjacentTiles = new List<GameObject>();
+        for (int i = 0; i < adjacentDirections.Length; i++)
+        {
+            adjacentTiles.Add(GetAdjacent(adjacentDirections[i]));
+        }
+        return adjacentTiles;
+    }
+
+    private List<GameObject> FindMatch(Vector2 castDir)
+    {
+        List<GameObject> matchingTiles = new List<GameObject>();
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir);
+        while (hit.collider != null && hit.collider.GetComponent<SpriteRenderer>().sprite == render.sprite)
+        {
+            matchingTiles.Add(hit.collider.gameObject);
+            hit = Physics2D.Raycast(hit.collider.transform.position, castDir);
+        }
+        return matchingTiles;
+    }
+
+    private void ClearMatch(Vector2[] paths)
+    {
+        List<GameObject> matchingTiles = new List<GameObject>();
+        for (int i = 0; i < paths.Length; i++) { matchingTiles.AddRange(FindMatch(paths[i])); }
+        if (matchingTiles.Count >= 2)
+        {
+            for (int i = 0; i < matchingTiles.Count; i++)
+            {
+                matchingTiles[i].GetComponent<SpriteRenderer>().sprite = null;
+            }
+            matchFound = true;
+        }
+    }
+
+    private bool matchFound = false;
+    public void ClearAllMatches()
+    {
+        if (render.sprite == null)
+            return;
+
+        ClearMatch(new Vector2[2] { Vector2.left, Vector2.right });
+        ClearMatch(new Vector2[2] { Vector2.up, Vector2.down });
+        if (matchFound)
+        {
+            render.sprite = null;
+            matchFound = false;
+            StopCoroutine(BoardGen.instance.FindNullTiles()); 
+            StartCoroutine(BoardGen.instance.FindNullTiles());
+            Score.instance.MoveCounter--;
+        }
+    }
+
 }
